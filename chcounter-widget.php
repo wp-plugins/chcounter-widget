1b4c12def7202eaf5958baeb86a2d9ffd90bd32a
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-18 21:16:03 +0000

    minor fix for WP_PLUGIN_DIR
    git-svn-id: https://plugins.svn.wordpress.org/chcounter-widget/trunk@74651 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- chcounter-widget.php
+++ chcounter-widget.php
@@ -52,6 +52,10 @@ class chCounterWidget
 			define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
 		if ( !defined( 'WP_PLUGIN_URL' ) )
 			define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+		if ( !defined( 'WP_CONTENT_DIR' ) )
+			define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+		if ( !defined( 'WP_PLUGIN_DIR' ) )
+			define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
 			
 		// Add trailing slash to $_SERVER['DOCUMENT_ROOT'] if it doesn't exist
 		if ( substr($_SERVER['DOCUMENT_ROOT'], -1, 1) != '/' )
@@ -427,7 +431,7 @@ register_activation_hook(__FILE__, array(&$chcounter_widget, 'init') );
 add_action( 'widgets_init', array(&$chcounter_widget, 'register') );
 add_action( 'admin_menu', array(&$chcounter_widget, 'addAdminMenu') );
 
-load_plugin_textdomain( 'chcounter', $path = PLUGINDIR.'/'.basename(__FILE__, ".php")  );
+load_plugin_textdomain( 'chcounter', $path = WP_PLUGIN_DIR.'/'.basename(__FILE__, ".php")  );
 
 if ( function_exists('register_uninstall_hook') )
    register_uninstall_hook(__FILE__, array(&$chcounter_widget, 'uninstall'));
